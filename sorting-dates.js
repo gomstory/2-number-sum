087a9223b46b68eae7f31b56b752f20460d9d471
Gom <arkom.madaehoh@gmail.com>
2019-10-04 19:30:28 +0700

    Update sorting-dates.js

--- sorting-dates.js
+++ sorting-dates.js
@@ -2,10 +2,10 @@
 // Dates data must be formated.
 const dates = [ "08 Dec 1990", "10", "29 Jan 1994", "24 Oct 2019", "11 Nov 2002", "14 Feb 2010", "14 Feb 1991"];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-const dateFormat = /(\d{2})\s(\w{3})\s(\d{4})/;
 const datePairs = new Map();
 
 // Filter invalid dates
+const dateFormat = /(\d{2})\s(\w{3})\s(\d{4})/;
 const validDates = dates.filter(date => dateFormat.test(date));
 
 for (let index = 0; index < validDates.length; index++){
