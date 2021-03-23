109528173a4feb6959c2c85725cbb83aba3c9e6e
Gom <arkom.madaehoh@gmail.com>
2021-03-23 18:37:03 +0700

    Update binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -1,15 +1,16 @@
-function binarySearch(arr, start, end, fine) {
-    if (end >= start) {
-        let mid = start + (end - start) / 2;
-        if (arr[mid] == fine) return mid;
-        if (arr[mid] > fine) return binarySearch(arr, start, mid - 1, fine);
-        return binarySearch(arr, mid + 1, end, fine);
-    }
+// Binary is O(log n) operation (time complexity)
+function binarySearch(arr, start, end, expected) {
+  if (end >= start) {
+      let mid = start + (end - start) / 2;
+      if (arr[mid] == expected) return mid;
+      if (arr[mid] > expected) return binarySearch(arr, start, mid - 1, expected);
+      return binarySearch(arr, mid + 1, end, expected);
+  }
 
-    return -1;
+  return -1;
 }
 
 let arr = [ 2, 4, 6, 8, 15, 24, 36, 72, 100 ];
-let fine = 36;
-let result = binarySearch(arr, 0, arr.length - 1, fine);
+let expected = 36;
+let result = binarySearch(arr, arr[0], arr.length - 1, expected);
 console.log("Result at index: " + result);
